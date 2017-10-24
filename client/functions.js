11edb67ca1ac25cb0cd9e53e95f932e8e9911675
Ilyasahmed197 <Ilyasahmed197@gmail.com>
2017-10-23 20:00:27 -0400

    functions.js, page.html updated

--- client/functions.js
+++ client/functions.js
@@ -1,1 +1,7 @@
-//functions file
+var canvas = document.getElementById('canvas1');
+
+var drawCanvas= function(){
+  var context = canvas.getContext('2d');
+  context.fillStyle= 'black';
+  context.fillRect(0,0,canvas.width,canvas.height); //erase canvas
+}
